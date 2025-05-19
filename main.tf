de5bf0f4e19cc7163b796a75ac3be79a2c0713d2
Chandra Reddy <121227250+chandra76768@users.noreply.github.com>
2025-05-20 00:29:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,23 +17,26 @@ data "aws_vpc" "default" {
   default = true
 }
 
-# Get the default subnet from the default VPC
-data "aws_subnet_ids" "default" {
-  vpc_id = data.aws_vpc.default.id
+# Get the default subnets from the default VPC
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
 
-# Use an existing key pair
-resource "aws_key_pair" "example_key" {
+# Use an existing key pair (no resource creation, just reference)
+data "aws_key_pair" "example_key" {
   key_name = "example-key"
 }
 
 # EC2 instance using default VPC and subnet
 resource "aws_instance" "example_ec2" {
-  ami             = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI
-  instance_type   = "t2.micro"
-  subnet_id       = element(data.aws_subnet_ids.default.ids, 0)  # First default subnet
+  ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI
+  instance_type = "t2.micro"
+  subnet_id     = element(data.aws_subnets.default.ids, 0)  # First default subnet
   security_groups = ["default"]  # Use default security group
-  key_name        = aws_key_pair.example_key.key_name
+  key_name      = data.aws_key_pair.example_key.key_name
 
   tags = {
     Name = "example-ec2-instance"

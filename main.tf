63a7c060652573a2e2c863e64c118e0a73a3e6ea
Chandra Reddy Cheruku <chandrareddy.cheruku@keyloop.com>
2025-05-19 15:37:36 +0530

    Initial commit

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+  required_version = ">= 1.5.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-terraform-bucket-${random_id.bucket_id.hex}"
+}
+
+resource "random_id" "bucket_id" {
+  byte_length = 4
+}

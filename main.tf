1db511be1453f4eea61e29d315aa5948372afc94
Chandra Reddy <121227250+chandra76768@users.noreply.github.com>
2025-05-19 23:29:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,10 +12,67 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "my-terraform-bucket-${random_id.bucket_id.hex}"
+resource "aws_vpc" "example_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "example-vpc"
+  }
 }
 
-resource "random_id" "bucket_id" {
-  byte_length = 4
+resource "aws_subnet" "example_subnet" {
+  vpc_id            = aws_vpc.example_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "example-subnet"
+  }
+}
+
+resource "aws_security_group" "example_sg" {
+  vpc_id = aws_vpc.example_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "example-sg"
+  }
+}
+
+resource "aws_key_pair" "example_key" {
+  key_name   = "example-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "aws_instance" "example_ec2" {
+  ami             = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI
+  instance_type   = "t2.micro"
+  subnet_id       = aws_subnet.example_subnet.id
+  security_groups = [aws_security_group.example_sg.name]
+  key_name        = aws_key_pair.example_key.key_name
+
+  tags = {
+    Name = "example-ec2-instance"
+  }
 }

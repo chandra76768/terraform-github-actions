181bc5a62f7cb639e91785c80df757c447916112
Chandra Reddy <121227250+chandra76768@users.noreply.github.com>
2025-05-20 09:50:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 4.0"
     }
   }
@@ -12,83 +12,65 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_vpc" "example_vpc" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-  tags = {
-    Name = "example-vpc"
+# Use the default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Get the default subnets from the default VPC
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
   }
 }
 
-resource "aws_subnet" "example_subnet" {
-  vpc_id                 = aws_vpc.example_vpc.id
-  cidr_block             = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-  availability_zone      = "us-east-1a"
-  tags = {
-    Name = "example-subnet"
-  }
-}
-
-resource "aws_security_group" "example_sg" {
-  vpc_id = aws_vpc.example_vpc.id
+# Create a new security group in the default VPC
+resource "aws_security_group" "de" {
+  name        = "de"
+  description = "Security group def for example EC2"
+  vpc_id      = data.aws_vpc.default.id
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]  # Allow SSH from anywhere (adjust for your security)
   }
 
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]  # Allow HTTP from anywhere
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]  # Allow all outbound
   }
 
   tags = {
-    Name = "example-sg"
+    Name = "de"
   }
 }
 
-resource "tls_private_key" "example_key" {
-  algorithm = "RSA"
-  rsa_bits  = 2048
+# Use an existing key pair
+data "aws_key_pair" "example_key" {
+  key_name = "example-key"
 }
 
-resource "aws_key_pair" "example_key" {
-  key_name   = "example-key"
-  public_key = tls_private_key.example_key.public_key_openssh
-}
-
-resource "aws_instance" "example_ec2" {
-  ami             = "ami-084568db4383264d4"  # Amazon Linux 2 AMI
-  instance_type   = "t2.micro"
-  subnet_id       = aws_subnet.example_subnet.id
-  security_groups = [aws_security_group.example_sg.name]
-  key_name        = aws_key_pair.example_key.key_name
+# EC2 instance using default VPC, subnet, and new security group
+resource "aws_instance" "example_ec21" {
+  ami                  = "ami-084568db4383264d4"  # Amazon Linux 2 AMI
+  instance_type        = "t2.micro"
+  subnet_id            = element(data.aws_subnets.default.ids, 0)
+  vpc_security_group_ids = [aws_security_group.de.id]
+  key_name             = data.aws_key_pair.example_key.key_name
 
   tags = {
-    Name = "example-ec2-instance"
+    Name = "example-ec21-instance"
   }
-}
-
-output "public_key" {
-  description = "Public key for SSH access"
-  value       = tls_private_key.example_key.public_key_openssh
-}
-
-output "private_key" {
-  description = "Private key for SSH access"
-  value       = tls_private_key.example_key.private_key_pem
-  sensitive   = true
 }

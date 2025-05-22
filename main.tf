1a929f637e402156a0a86fc1640eaa981a802d4c
Chandra Reddy <121227250+chandra76768@users.noreply.github.com>
2025-05-22 12:09:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,24 @@
+### Root Module - main.tf
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+data "aws_key_pair" "example_key" {
+  key_name = "example-key"
+}
+
 resource "aws_security_group" "de" {
   name        = "de"
   description = "Security group def for example EC2"
@@ -29,31 +50,31 @@ resource "aws_security_group" "de" {
   }
 }
 
-locals {
-  ec2_instances = {
-    abc = {
-      ami            = "ami-084568db4383264d4"
-      instance_type  = "t2.micro"
-      subnet_index   = 0
-      instance_name  = "abc"
-    },
-    abc2 = {
-      ami            = "ami-084568db4383264d4"
-      instance_type  = "t2.micro"
-      subnet_index   = 1
-      instance_name  = "abc2"
-    }
+module "example_ec2" {
+  source             = "./modules/ec2"
+  ami                = "ami-084568db4383264d4"
+  instance_type      = "t2.micro"
+  subnet_id          = element(data.aws_subnets.default.ids, 0)
+  security_group_ids = [aws_security_group.de.id]
+  key_name           = data.aws_key_pair.example_key.key_name
+  instance_name      = "example-single-instance"
+}
+
+### Root Module - variables.tf (optional)
+# No variables needed at root since values are hardcoded in main.tf
+
+
+### Module - modules/ec2/main.tf
+
+resource "aws_instance" "this" {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = var.security_group_ids
+  key_name               = var.key_name
+
+  tags = {
+    Name = var.instance_name
   }
 }
 
-module "ec2_instances" {
-  source  = "./modules/ec2"
-  for_each = local.ec2_instances
-
-  ami                = each.value.ami
-  instance_type      = each.value.instance_type
-  subnet_id          = element(data.aws_subnets.default.ids, each.value.subnet_index)
-  security_group_ids = [aws_security_group.de.id]
-  key_name           = data.aws_key_pair.example_key.key_name
-  instance_name      = each.value.instance_name
-}
